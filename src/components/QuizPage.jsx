1f67f75fb9b94f9d0f61ee8d9e4d725c624f9876
lesley_w <lrwesley92@gmail.com>
2024-01-24 22:16:39 -0600

    Fixed issue of Too Many Requests error with API by removing StrictMode from main.jsx

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -5,15 +5,14 @@ function QuizPage() {
 
   const [questionsData, setQuestionsData] = React.useState({})
 
-  React.useEffect(function() {
-      fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-        .then(res => res.json())
-        .then(data => setQuestionsData(data))
+  React.useEffect(() => {
+      fetch("https://opentdb.com/api.php?amount=5").then(res => res.json()).then(data => console.log(data))
   }, [])
+
 
   return (
     <div className="quiz-page-container">
-      <pre>{JSON.stringify(questionsData, null, 2)}</pre>
+        
     </div>
   )
 }
