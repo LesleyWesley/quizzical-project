65f504c312fb0ce44c9b66d61b2104c437713435
lesley_w <lrwesley92@gmail.com>
2024-01-25 14:04:14 -0600

    Changed active states so hover styling is used when element is hovered or focused on, instead of

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -5,14 +5,16 @@ function QuizPage() {
 
   const [questionsData, setQuestionsData] = React.useState({})
 
-  React.useEffect(() => {
-      fetch("https://opentdb.com/api.php?amount=5").then(res => res.json()).then(data => console.log(data))
+  React.useEffect(function() {
+      console.log("effect ran")
+      fetch("https://opentdb.com/api.php?amount=5").then(res => res.json()).then(data => setQuestionsData(data))
   }, [])
+
 
 
   return (
     <div className="quiz-page-container">
-        
+        <Question />
     </div>
   )
 }
