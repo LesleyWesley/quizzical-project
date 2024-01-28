1f0425f0f922fd6cda9a6687a8a7c28cd96f7e08
lesley_w <lrwesley92@gmail.com>
2024-01-28 00:38:10 -0600

    Decoded api data so extra characters don't appear in quiz

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { decode } from "html-entities"
 
 function Question(props) {
 
@@ -6,28 +7,33 @@ function Question(props) {
   const randomI = Math.floor(Math.random() * 4);
   const allAnswersArray = []
   for (let i = 0; i < props.incorrect.length; i++) {
-    allAnswersArray.push(props.incorrect[i])
+    allAnswersArray.push(decode(props.incorrect[i]))
   }
-  allAnswersArray.splice(randomI, 0, props.correct)
+  allAnswersArray.splice(randomI, 0, decode(props.correct))
 
   //Maps over allAnswersArray to return a radio input for each option
+  const optionElements = allAnswersArray.map( option => {
+    return (
+      <label>
+          <input
+            type="radio"
+            id={option} //needs a prop
+            name="option" //needs to be different for every question
+            value={option} //needs a prop
+            //needs a checked value once state is written
+            //needs an onChange value once state is written
+          />
+          {option}
+      </label>
+    )
+  })
 
 
   return (
     <div className="question-container">
-      <h2>{props.question}</h2>
+      <h2>{decode(props.question)}</h2>
       <div className="options-container">
-          <label>
-              <input
-                type="radio"
-                id="Cabbage Patch Kids" //needs a prop
-                name="option" //needs to be different for every question
-                value="Cabbage Patch Kids" //needs a prop
-                //needs a checked value once state is written
-                //needs an onChange value once state is written
-              />
-              Cabbage Patch Kids
-          </label>
+          {optionElements}
       </div>
     </div>
   )
